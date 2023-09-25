74014320e01c2874d036badb2774b0365ab2c497
AlejandroAlbiter <alejandro.albiter@corvusdata.mx>
2023-09-25 14:41:25 -0600

    Modificacion del nombre del objeto suv

--- Alejandro/myClass.java
+++ Alejandro/myClass.java
@@ -1,16 +1,16 @@
 public class myClass {
     public static void main(String[] args){
-        suv Carro1 = new suv("BMW", "X5", "Azul");
+        suv Camioneta = new suv("BMW", "X5", "Azul");
         hothatch Carro2 = new hothatch();
         
-        Carro1.Acelerar();
+        Camioneta.Acelerar();
         Carro2.setMarca("Cupra");
         Carro2.setModelo("Formentor");
         Carro2.setColor("Gris Oxford");
 
         Carro2.Acelerar();
-        Carro1.Frenar();
-        Carro1.Informacion();
+        Camioneta.Frenar();
+        Camioneta.Informacion();
         Carro2.Frenar();
         Carro2.Informacion();
 
