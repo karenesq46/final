bec1fdc0a1cd2dfbdba91b7e5e91bb4cee474f22
kikiubo <kikiubo@hotmail.com>
2023-09-25 11:58:49 -0600

    Modificacion en nombre del objeto

--- Lalo/Alumno/src/main/java/Main.java
+++ Lalo/Alumno/src/main/java/Main.java
@@ -1,7 +1,7 @@
 
 public class Main {
     public static void main(String[] args) {
-        Alumno juanPerez = new Alumno("Juan Perez", 24, "juan@hotmail.com");
+        Alumno estebanPerez = new Alumno("Juan Perez", 24, "juan@hotmail.com");
         System.out.println(juanPerez.getNombre() + juanPerez.getEdad() + juanPerez.getCorreo());
         }
     }
