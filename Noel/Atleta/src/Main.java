22afd30af84e83a8d2c6f74fa2efe63bb552f8be
noelsilis <noelsilis99@gmail.com>
2023-09-25 11:33:15 -0600

    Modificacion del objeto 'a'

--- Noel/Atleta/src/Main.java
+++ Noel/Atleta/src/Main.java
@@ -1,11 +1,11 @@
 import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
-        Atleta a = new Atleta(1,"Christian",2);
+        Atleta obj_atleta = new Atleta(1,"Christian",2);
         ArrayList<Atleta> atletas = new ArrayList<Atleta>();
 
         for (int i = 0; i < 5; i++) {
-            atletas.add(a);
+            atletas.add(obj_atleta);
         }
         for(Atleta atleta: atletas) {
             System.out.println(atleta.getDatosAtleta());
