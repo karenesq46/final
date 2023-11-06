bbb0f0b757ec47800b08a762f6175154905e11c3
elSzs4 <145390638+elSzs4@users.noreply.github.com>
2023-11-06 13:37:45 -0600

    Add files via upload
    mi repo

---
+++ crud_vbc/crud_vbc/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path, include
+from django.conf import settings
+
+urlpatterns = [
+    path('', include ('app_vbc.urls'))
+     ]
+if settings.DEBUG:
+  from django.conf.urls.static import static
+  urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
